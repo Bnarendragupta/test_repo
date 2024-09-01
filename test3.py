8172131281ed2350b227257b99f043c5c4f7da54
narendra <nanduboddu.nb@gmail.com>
2024-09-01 15:42:19 +0530

    first commit

---
+++ test3.py
@@ -0,0 +1,14 @@
+import sys
+
+# Define your variables
+a = 1
+b = 2
+c = 2
+d = 3
+# Check conditions
+if a == b and c == d:
+    # If both conditions are true, exit with status 0 (success)
+    sys.exit(0)
+else:
+    # If either condition is false, exit with status 1 (failure)
+    sys.exit(1)
